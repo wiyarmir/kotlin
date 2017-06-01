bd251dbd0c4e16bbb58a10ea00bd64516dca2f32
Alexander Udalov <Alexander.Udalov@jetbrains.com>
2017-06-01 19:42:59 +0300

    Support reflection for local delegated properties
     #KT-15222 Fixed

--- core/reflection.jvm/src/kotlin/reflect/jvm/internal/KClassImpl.kt
+++ core/reflection.jvm/src/kotlin/reflect/jvm/internal/KClassImpl.kt
@@ -28,8 +28,12 @@ import org.jetbrains.kotlin.name.ClassId
 import org.jetbrains.kotlin.name.Name
 import org.jetbrains.kotlin.resolve.DescriptorUtils
 import org.jetbrains.kotlin.resolve.scopes.MemberScope
+import org.jetbrains.kotlin.serialization.deserialization.MemberDeserializer
+import org.jetbrains.kotlin.serialization.deserialization.descriptors.DeserializedClassDescriptor
+import org.jetbrains.kotlin.serialization.jvm.JvmProtoBuf
 import kotlin.jvm.internal.TypeIntrinsics
 import kotlin.reflect.*
+import kotlin.reflect.jvm.deserializeToDescriptor
 import kotlin.reflect.jvm.internal.KDeclarationContainerImpl.MemberBelonginess.DECLARED
 import kotlin.reflect.jvm.internal.KDeclarationContainerImpl.MemberBelonginess.INHERITED
 
@@ -182,6 +186,14 @@ internal class KClassImpl<T : Any>(override val jClass: Class<T>) : KDeclaration
     override fun getFunctions(name: Name): Collection<FunctionDescriptor> =
             memberScope.getContributedFunctions(name, NoLookupLocation.FROM_REFLECTION) +
             staticScope.getContributedFunctions(name, NoLookupLocation.FROM_REFLECTION)
+
+    override fun getLocalProperty(index: Int): PropertyDescriptor? {
+        return (descriptor as? DeserializedClassDescriptor)?.let { descriptor ->
+            val proto = descriptor.classProto.getExtension(JvmProtoBuf.classLocalVariable, index)
+            val nameResolver = descriptor.c.nameResolver
+            deserializeToDescriptor(jClass, proto, nameResolver, descriptor.c.typeTable, MemberDeserializer::loadProperty)
+        }
+    }
 
     override val simpleName: String? get() = data().simpleName
 
