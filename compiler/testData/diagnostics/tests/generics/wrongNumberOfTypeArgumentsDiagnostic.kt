667d5ca31de64480d56f90749a8316f8cb57e1df
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2017-06-19 20:35:00 +0300

    Report about wrong number of type arguments instead of ambiguity
     #KT-7975 Fixed
     #KT-1809 Fixed

---
+++ compiler/testData/diagnostics/tests/generics/wrongNumberOfTypeArgumentsDiagnostic.kt
@@ -0,0 +1,14 @@
+// !DIAGNOSTICS: -UNUSED_PARAMETER, -UNUSED_VARIABLE
+
+fun myFun(i : String) {}
+fun myFun(i : Int) {}
+
+fun test1() {
+    myFun<!WRONG_NUMBER_OF_TYPE_ARGUMENTS!><Int><!>(3)
+}
+
+fun test2() {
+    val m0 = java.util.<!TYPE_INFERENCE_NO_INFORMATION_FOR_PARAMETER!>HashMap<!>()
+    val m1 = java.util.HashMap<!WRONG_NUMBER_OF_TYPE_ARGUMENTS!><String, String, String><!>()
+    val m2 = java.util.HashMap<!WRONG_NUMBER_OF_TYPE_ARGUMENTS!><String><!>()
+}
